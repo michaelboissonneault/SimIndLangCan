437c3df0e70114e2015772fe1dc95887e243f019
michaelboissonneault <boissonneault@nidi.nl>
2022-02-16 09:22:44 +0100

    I added a routine to assign languages to a discrete number of latent pathways of change using the lcmm packages (growth mixture modeling).

--- 6.Results.R
+++ 6.Results.R
@@ -9,10 +9,14 @@ library(MicSim)
 library(lubridate)
 library(openxlsx)
 library(cowplot)
+library(lcmm)
 
 rm(list=ls())
 
 setwd("C:/Users/micha/Documents/Git-RStudio/SimIndLangCan")
+
+#set ggplot2 theme for consistency
+theme_set(theme_bw())  
 
 #starting populations
 sp <- readRDS("startingpopulations")
@@ -25,6 +29,7 @@ int.nb <- readRDS("inttrans")
 
 #simulation results
 results <- readRDS("first25_30runs")
+
 ################################################################################
 #LANGUAGE PROFILES
 ################################################################################
@@ -113,3 +118,119 @@ plots <- function(n){
 lapply(1:25,function(x) plots(x))
 
 #######################################################################################################
+############################################################
+#Estimate trajectories
+############################################################
+#average across runs and inital speaker number in the year 2016
+trajectories <- left_join(simres %>% group_by(period5,language) %>% summarise(average=mean(alive)),
+                        simres %>% group_by(language) %>% filter(period==2016) %>% summarise(alive2016=mean(alive)))
+
+#proportion of initial number over time
+trajectories$proportion <- round(trajectories$average / trajectories$alive2016 *100, 1)
+
+#add language number
+trajectories$nb <- rep(1:25,85/5)
+
+#reconvert year to years after 2016
+trajectories$period0 <- trajectories$period5-2016
+
+#visualize pathways
+ggplot(trajectories,aes(period5,proportion,group=nb))+
+  geom_line()
+
+#model with splines and 3 groups
+model3splines <- lcmm(proportion ~ period0, random = ~ period0, subject = 'nb', 
+                      mixture = ~ period0, ng = 3, idiag = TRUE, data=trajectories,
+                      link="splines")
+
+#mixutre model with splines and 4 groups
+model4splines <- lcmm(proportion ~ period0, random = ~ period0, subject = 'nb', 
+                      mixture = ~ period0, ng = 4, idiag = TRUE, data=trajectories,
+                      link="splines")
+
+#mixutre model with splines and 5 groups
+model5splines <- lcmm(proportion ~ period0, random = ~ period0, subject = 'nb', 
+                      mixture = ~ period0, ng = 5, idiag = TRUE, data=trajectories,
+                      link="splines")
+
+#Compare models 
+model3splines
+model4splines
+model5splines
+
+#the model with 3 groups fits best; put in data frame
+results <- data.frame(period0=seq(0,80,5))
+results.raw <- predictY(model3splines, newdata=results, var.time="period0", draws = T)
+results <- data.frame(period5=rep(seq(2016,2096,5),3),
+                      predicted=results.raw$pred[1:(17*3)],
+                      lower=results.raw$pred[(17*3+1):(17*3*2)],
+                      upper=results.raw$pred[(17*3*2+1):(17*3*3)],
+                      group=rep(c("1","2","3"),each=17))
+
+#assign the groups in the initial data
+trajectories$group <- as.character(rep(model3splines$pprob[,2],17))
+
+#merge the two sets
+results <- left_join(results,select(trajectories,period5,proportion,group,nb))
+
+#plot
+ggplot(results)+
+  geom_line(aes(period5,predicted,group=group,color=group),size=1)+
+  geom_line(aes(period5,proportion,group=nb),color="grey")+
+  geom_line(aes(period5,lower,group=group,color=group),linetype="dashed")+
+  geom_line(aes(period5,upper,group=group,color=group),linetype="dashed")+
+  ylab("Change in the number of speakers \n (proportional to the initial population size)")+
+  xlab("Year")
+
+#breakdown by group        
+table(model3splines$pprob[,2])
+
+#######################################################################################
+#Attempt considering each run for each language
+#######################################################################################
+#new data set with proportion of speakers to number in 2016
+trajectories <- simres %>% ungroup() %>% select(-period) %>% distinct(period5,language,run,.keep_all=T)
+initial <- simres %>% ungroup() %>% filter(period==2016) %>% rename(initial=alive) %>% select(language,initial) %>% distinct()
+trajectories <- left_join(trajectories,initial,by="language")
+trajectories$proportion <- trajectories$alive/trajectories$initial
+
+#add language number
+trajectories$nb <- rep(1:(25*30),17)
+
+#reconvert year to years after 2016
+trajectories$period0 <- trajectories$period5-2016
+
+ggplot(trajectories,aes(period5,proportion,group=nb))+
+  geom_line()
+
+#model with splines and 3 groups
+model3splines <- lcmm(proportion ~ period0, random = ~ period0, subject = 'nb', 
+                      mixture = ~ period0, ng = 3, idiag = TRUE, data=trajectories,
+                      link="splines")
+
+#put in data frame
+results <- data.frame(period0=seq(0,80,5))
+results.raw <- predictY(model3splines, newdata=results, var.time="period0", draws = T)
+results <- data.frame(period5=rep(seq(2016,2096,5),3),
+                      predicted=results.raw$pred[1:(17*3)],
+                      lower=results.raw$pred[(17*3+1):(17*3*2)],
+                      upper=results.raw$pred[(17*3*2+1):(17*3*3)],
+                      group=rep(c("1","2","3"),each=17))
+
+#assign the groups in the initial data
+trajectories$group <- as.character(rep(model3splines$pprob[,2],17))
+
+#merge the two sets
+results <- left_join(results,select(trajectories,period5,proportion,group,nb))
+
+#plot
+ggplot(results)+
+  geom_line(aes(period5,predicted,group=group,color=group),size=1)+
+  geom_line(aes(period5,proportion,group=nb),color="grey")+
+  geom_line(aes(period5,lower,group=group,color=group),linetype="dashed")+
+  geom_line(aes(period5,upper,group=group,color=group),linetype="dashed")+
+  ylab("Change in the number of speakers \n (proportional to the initial population size)")+
+  xlab("Year")
+
+#breakdown by group        
+table(model3splines$pprob[,2])
